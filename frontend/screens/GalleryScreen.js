87a16f393de22a572f3f801c338fc390c4357416
Laetitia Langlois <weblaetitia@hotmail.com>
2020-08-05 17:09:19 +0200

    loop on photos array and display them on gallery screen

--- frontend/screens/GalleryScreen.js
+++ frontend/screens/GalleryScreen.js
@@ -7,64 +7,21 @@ import {connect} from 'react-redux'
 
 function GalleryScreen(props) {
 
-  console.log(props.photos)
-
-    const myGallery = [
-        { picture: '../assets/picture-1.jpg',
-        tags: ['homme', '70 ans', 'barbe', 'joyeux', 'cheveux gris']
-         },
-        { picture: '../assets/picture-2.jpg',
-        tags: ['femme', '31 ans', 'lunettes', 'joyeux', 'cheveux chatain']
-         },
-        { picture: '../assets/picture-3.jpg',
-        tags: ['homme', '26 ans', 'lunettes', 'joyeux', 'noeud papillon', 'jaune']
-        },
-        { picture: '../assets/picture-4.jpg',
-        tags: ['femme', '70 ans', 'lunettes', 'joyeux', 'collier', 'cheveux gris']
-        },
-    ]
-
-    const cards = myGallery.map((element, i) => {
-        return (
-            <Card key={i} 
-            image={require('../assets/picture-4.jpg')}>
-            <Badge value={element.tags} status="success" />
-          </Card>
-        )
-    })
+  // loop card on photos' array
+  var photoCards = props.photos.map((element, i) => {
+    return (
+      <Card key={i} 
+            image={{uri: element}}>
+        <Badge value='photo' status="success" />
+      </Card>
+    )
+  })
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
         <Text h1 style={styles.h1}>John's gallery</Text>
-        <Card image={require('../assets/picture-1.jpg')}>
-            <Badge value='homme' status="success" />
-            <Badge value='70 ans' status="success" />
-            <Badge value='barbe' status="success" />
-            <Badge value='joyeux' status="success" />
-            <Badge value='cheveux gris' status="success" />
-        </Card>
-        <Card image={require('../assets/picture-2.jpg')}>
-            <Badge value='femme' status="success" />
-            <Badge value='31 ans' status="success" />
-            <Badge value='lunettes' status="success" />
-            <Badge value='joyeux' status="success" />
-            <Badge value='cheveux chatains' status="success" />
-        </Card>
-        <Card image={require('../assets/picture-3.jpg')}>
-            <Badge value='homme' status="success" />
-            <Badge value='26 ans' status="success" />
-            <Badge value='noeud papillon' status="success" />
-            <Badge value='joyeux' status="success" />
-            <Badge value='cheveux noirs' status="success" />
-        </Card>
-        <Card image={require('../assets/picture-4.jpg')}>
-            <Badge value='femme' status="success" />
-            <Badge value='66 ans' status="success" />
-            <Badge value='collier' status="success" />
-            <Badge value='joyeux' status="success" />
-            <Badge value='cheveux gris' status="success" />
-        </Card>
+        {photoCards}
       </ScrollView>
     </SafeAreaView>
   )
